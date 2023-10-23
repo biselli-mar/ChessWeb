1f63641e1d639241c2e56c45c6e26ccffa695026
biselli-mar <ma741bis@htwg-konstanz.de>
2023-10-23 15:58:49 +0200

    place dependencies in outsourced file

---
+++ project/dependencies.scala
@@ -0,0 +1,15 @@
+import sbt._
+import Keys._
+import sbt.librarymanagement.InclExclRule
+
+object dependencies {
+  val scala2Version = "2.13.10"
+  val scala3Version = "3.2.2"
+
+  val scalaTest = "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+  val scalaTest3 = "org.scalatest" %% "scalatest" % "3.2.10" % "test"
+
+  val commonDependency = Seq(
+    scalaTest,
+  )
+}
