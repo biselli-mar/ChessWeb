0798b404fde7c36295559fd1d6d538b68d7fcf1c
biselli-mar <ma741bis@htwg-konstanz.de>
2023-10-23 16:09:34 +0200

    remove tutorial comments

--- app/controllers/ChessController.scala
+++ app/controllers/ChessController.scala
@@ -56,13 +56,6 @@ class ChessController @Inject()(ws: WSClient, config: Configuration, val control
 
   def backToPlay(response: WSResponse) = SeeOther("/play")
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
