178ef7233099e908c7f3e29f40658c4db8637f94
biselli-mar <ma741bis@htwg-konstanz.de>
2023-11-13 19:52:10 +0100

    Dark mode switch and better padding

--- app/models/data/NavItem.scala
+++ app/models/data/NavItem.scala
@@ -9,6 +9,7 @@ case class NavItem(
 
 object NavItem {
   val items = Seq(
+    NavItem("Play", "bi-dpad", "/play"),
     NavItem("About", "bi-info-square", "/about"),
     NavItem("Profile", "bi-person-square", "/profile"),
   )
