e66c0eb60559b7961ae4e42554ab1a66cf4f61a2
biselli-mar <marcelbiselli@gmx.de>
2023-12-04 18:26:37 +0100

    properly display coordinates

--- app/models/data/NavItem.scala
+++ app/models/data/NavItem.scala
@@ -9,7 +9,6 @@ case class NavItem(
 
 object NavItem {
   val items = Seq(
-    NavItem("Play", "bi-dpad", "/play"),
     NavItem("About", "bi-info-square", "/about"),
     NavItem("Profile", "bi-person-square", "/profile"),
   )
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -232,6 +232,8 @@ function fillBoard(position, playerColor) {
         chessBoard.append(pieceDiv(piece, tile));
     }
 
+    createCoordinateSvg(playerColor === 'w');
+
     const pieces = $('.piece');
 
     pieces.on('mousedown', pieceMousedownHandler);
@@ -245,6 +247,48 @@ function fillBoard(position, playerColor) {
     });
 
     chessBoard.removeClass('visually-hidden');
+}
+
+function createCoordinateSvg(forWhite) {
+    const svg = $('#chess-coordinates')[0];
+    const files = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    const fileSvg = (x, y, text, color) => {
+        const fileText = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+        fileText.setAttribute('x', x + '%');
+        fileText.setAttribute('y', y + '%');
+        fileText.setAttribute('class', 'coordinates-' + color + ' file');
+        fileText.textContent = text;
+        svg.appendChild(fileText);
+        //svg.append(`<text x="${x}%" y="${y}%" class="coordinates-${color} file">${text}</text>`);
+    }
+    const rankSvg = (x, y, text, color) => {
+        const rankText = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+        rankText.setAttribute('x', x + '%');
+        rankText.setAttribute('y', y + '%');
+        rankText.setAttribute('class', 'coordinates-' + color + ' rank');
+        rankText.textContent = text;
+        svg.appendChild(rankText);
+        //svg.append(`<text x="${x}%" y="${y}%" class="coordinates-${color} rank">${text}</text>`);
+    }
+    for(let i = 0; i < 8; i++) {
+        let file = files[i];
+        if (!forWhite) {
+            file = files[7 - i];
+        }
+        let rank = 8 - i;
+        if (!forWhite) {
+            rank = i + 1;
+        }
+        let fileColor = 'light';
+        let rankColor = 'dark';
+        if (i % 2 == 0) {
+            fileColor = 'dark';
+            rankColor = 'light';
+        }
+
+        fileSvg(i * 12.5 + 10.8, 99, file.toString(), fileColor);
+        rankSvg(0.75, i * 12.5 + 3.5, rank.toString(), rankColor);
+    }
 }
 
 function processMove(gameData) {
